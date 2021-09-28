ccb1457d7f1ce64e6ed6e143cf3ed935bb044401
Julie <yulia567@icloud.com>
2021-09-28 23:42:43 +0300

    lesson 8

---
+++ Lesson8/task.js
@@ -0,0 +1,23 @@
+// Начальник цеха пригласил людей на совещание
+// Каждый, кто входит в кабинет пожимает руки всем присутствующим
+// Сколько человек зашло в кабинет, если известно, что всего произошло 120 рукопожатий.
+
+
+function getPeople(handshake) {
+    let peoples = 0;
+    let currentHandshake = 0;
+
+    do {
+        peoples++;
+        currentHandshake += peoples;
+    } while (currentHandshake < handshake);
+
+    return peoples;
+}
+
+console.log( getPeople(1) ); // 1
+console.log( getPeople(3) ); // 2
+console.log( getPeople(6) ); // 3
+console.log( getPeople(10) ); // 4
+console.log( getPeople(15) ); // 5
+console.log( getPeople(120) ); // 15
